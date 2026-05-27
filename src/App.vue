01d2d15cca7a9e1860266cd1989230387c0fdc09
Jiro <jaredazul1145@gmail.com>
2026-05-27 00:00:40 -0500

    Configuracion inicial del proyecto Vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <main class="app">
+    <section class="intro">
+      <h1>CRUD de Posts con Vue</h1>
+      <p>Vue 3 + Composition API + Vue Router + json-server</p>
+    </section>
+  </main>
 </template>
